758f0a48eb8b86d2fb5aa541df6d9d7691770abd
Camila Bosa <camila.bosa@indicium.tech>
2022-04-01 16:42:49 -0300

    atualizaçao das tabelas stg_vendors, stg_produtcs e source_raw

--- dbt_analytics/models/staging/stg_vendors.sql
+++ dbt_analytics/models/staging/stg_vendors.sql
@@ -1,11 +1,23 @@
 with source_vendor as(
-	select
-		name
-        , creditrating
-        , preferredvendorstatus 
-        , activeflag
-        , modifieddate	
-	from purchasing.vendor
+		select 
+        name as company_name
+		, case 
+			when cast(creditrating as integer) = 1 then 'Superior'
+			when cast(creditrating as integer) = 2 then 'Excellent'
+			when cast(creditrating as integer) = 3 then 'Above average'
+			when cast(creditrating as integer) = 4 then 'Average'
+			when cast(creditrating as integer) = 5 then 'Below average'
+		end as credit_rating
+		, case 
+			when cast(preferredvendorstatus as integer) = 0 then 'Preffered' 
+			when cast(preferredvendorstatus as integer) = 1 then 'NotPreffered'
+		end as preffered_vendor
+		, case 
+			when cast(activeflag as integer) = 0 then 'NotActive' 
+			when cast(activeflag as integer) = 1 then 'Active'
+		end as vendor_active_used
+		, cast(modifieddate	as date)
+	from {{ source('analytics', 'vendor')}}
 )
 select *
 from source_vendor
