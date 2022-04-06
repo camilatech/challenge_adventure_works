80891ffe19f6da91eb6612e77ecad949449f8d57
Camila Bosa <camila.bosa@indicium.tech>
2022-04-06 02:47:31 -0300

    modificações do Gabriel (add source_raw_products)

--- dbt_analytics/models/marts/fct_purchases.sql
+++ dbt_analytics/models/marts/fct_purchases.sql
@@ -1,128 +1,125 @@
 
 {{ config(materialized='table') }}
 
+
 with
     fk_products as(
-        select
-            productid
-            , sk_product
-        from {{ref('stg_products')}}
-    ),
-    fk_ship as(
-        select
-            sk_ship
-            , ship_method_id as ship_id
-        from {{ref('stg_shippings')}}
-    ),
-    fk_vendor as(
-        select
-            sk_vendor
-            , vendorid
-        from {{ref('stg_vendors')}}
-    )
-    , purchasing as (
-        select 
-            unit_price
-            , order_qty
-            , received_qty
-            , rejected_qty
-            , due_date
-            , purchase_status
-            , tax_amount
-            , subtotal
-            , vendor_id
-            , employee_id
-            , ship_method_id
-            , order_date
-            , ship_date
-            , product_id
-            , purchase_detail_id
-            , sk_ship
-        from {{ref('stg_purchases')}}
-            left join fk_ship
-            on fk_ship.ship_id = ship_method_id
-    )
+       select
+           productid
+           , sk_product
+       from {{ref('stg_products')}}
+   ),
 
-select * 
-from purchasing
+   fk_ship as(
+       select
+           sk_ship
+           , ship_method_id as ship_id
+       from {{ref('stg_shipping')}}
+   ),
 
-   -- purchase_joined_ship as (
+   fk_vendor as(
+       select
+           sk_vendor
+           , vendorid
+       from {{ref('stg_vendors')}}
+   ),
 
-   --     select *
-
-   --     from purchasing
-
-   --         left join fk_ship
-
-   --         on fk_ship.ship_id = purchasing.ship_method_id
-
-   -- ),
+   fk_employee as (
+       select
+           sk_employee
+           , employee_id
+       from {{ref('stg_employee')}}
+   ),
 
 
-   -- purchase_joined_vendor as (
+   purchasing as (
+       select
+           unit_price
+           , order_qty
+           , received_qty
+           , rejected_qty
+           , due_date
+           , purchase_status
+           , tax_amount
+           , subtotal
+           , vendor_id
+           , employee_id as employee
+           , ship_method_id
+           , order_date
+           , ship_date
+           , product_id
+           , purchase_detail_id
+           , sk_ship
+           , (unit_price*order_qty) as total_item
+       from {{ref('stg_fct_purchasing')}}
+           left join fk_ship
+           on fk_ship.ship_id = ship_method_id
 
-    --    select *
+   ),
 
-    --    from purchase_joined_ship
+   purchasing_vendor as(
+       select * from purchasing
+       left join fk_vendor
+       on fk_vendor.vendorid = purchasing.vendor_id
+   ),
 
-    --    left join fk_vendor
+   purchasing_employee as(
+       select * from purchasing_vendor
+       left join fk_employee
+       on fk_employee.employee_id = purchasing_vendor.employee
+   ),
 
-    --        on fk_vendor.vendorid = purchase_joined_ship.vendor_id
+   
 
-   -- ),
+   purchasing_purchasing as(
+       select
+           unit_price
+           , order_qty
+           , received_qty
+           , rejected_qty
+           , due_date
+           , purchase_status
+           -- , tax_amount
+           -- , subtotal
+           -- , vendor_id
+           -- , employee_id as employee
+           -- , ship_method_id
+           , order_date
+           , ship_date
+           , product_id
+           , purchase_detail_id
+           , (unit_price*order_qty) as total_item
+           , sk_ship
+           , sk_vendor
+           , sk_employee
+       from purchasing_employee
+   ),
 
+   purchasing_final as(
+       select * from purchasing_purchasing
+       left join fk_products
+       on fk_products.productid = purchasing_purchasing.product_id
+   )
 
-
-    --purchase_joined_product as (
-
-    --    select *
-
-   --     from purchase_joined_vendor
-
-   --     left join fk_products
-
-   --         on fk_products.productid = purchase_joined_vendor.product_id
-
-   -- )
-
-    
-
-    --select 
-
-    --    unit_price
-
-    --    , order_qty
-
-    --    , received_qty
-
-    --    , rejected_qty
-
-    --    , due_date
-
-    --    , purchase_status
-
-    --    , purchase_detail_id
-
-    --    , tax_amount
-
-    --    , subtotal
-
-    --    , vendor_id
-
-    --    , employee_id
-
-    --    , ship_method_id
-
-    --    , order_date
-
-    --    , ship_date
-
-    --    , product_id 
-
-    --    , sk_product
-
-    --    , sk_ship
-
-    --    , sk_vendor 
-
-    --    from purchase_joined_product
+   select
+       unit_price
+           , order_qty
+           , received_qty
+           , rejected_qty
+           , due_date
+           , purchase_status
+           -- , tax_amount
+           -- , subtotal
+           -- , vendor_id
+           -- , employee_id as employee
+           -- , ship_method_id
+           , order_date
+           , ship_date
+           , product_id
+           , purchase_detail_id
+           , (unit_price*order_qty) as total_item
+           , sk_ship
+           , sk_vendor
+           , sk_employee
+           , sk_product
+   from purchasing_final
